48534d801ca3774603230d91275739737b763b1b
Benidict Dulce <benidict@metromart.com>
2026-01-10 21:05:52 +0800

    feat: create ship and display it to game window.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import  pygame
 from settings.settings import Settings
+from ship.ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
@@ -19,21 +20,24 @@ class AlienInvasion:
         #self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
 
+        # Display ship
+        self.ship = Ship(self)
+
         # Set the background color
-        self.bg_color = settings.bg_color
-        #self.bg_color = (230, 230, 230)
+        self.bg_color = (230, 230, 230)
 
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if even.type == pygame.QUIT:
-                    sys.exit()
+            #for event in pygame.event.get():
+                #if even.type == pygame.QUIT:
+                 #   sys.exit()
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
