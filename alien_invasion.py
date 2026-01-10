0b8d7b5f1c84413c5b9eaa5c1f4ca4cbdeaff6b8
Benidict Dulce <benidict@metromart.com>
2026-01-10 20:39:09 +0800

    feat: create AI game window canvas and settings class.

---
+++ alien_invasion.py
@@ -0,0 +1,45 @@
+import sys
+import  pygame
+from settings.settings import Settings
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+
+        #self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color
+        self.bg_color = settings.bg_color
+        #self.bg_color = (230, 230, 230)
+
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if even.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
